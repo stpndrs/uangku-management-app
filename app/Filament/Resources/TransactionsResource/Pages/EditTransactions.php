2c9a6156cb899bf98f838eefca093b761c0e4b03
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2023-12-24 22:58:33 +0800

    test: selisih transaksi

--- app/Filament/Resources/TransactionsResource/Pages/EditTransactions.php
+++ app/Filament/Resources/TransactionsResource/Pages/EditTransactions.php
@@ -28,44 +28,67 @@ class EditTransactions extends EditRecord
     {
         $saving = Savings::find($data['savings_id']);
 
-        if ($record->savings_id === $data['savings_id']) {
-            if ($record->price !== $data['price']) {
-                if ($record->price > $data['price']) {
-                    $saving->update([
-                        'remaining_money' => $saving->remaining_money + ($record->price - $data['price'])
-                    ]);
+        // if ($record->savings_id === $data['savings_id']) {
+        //     if ($record->price !== $data['price']) {
+        //         if ($record->price > $data['price']) {
+        //             $saving->update([
+        //                 'remaining_money' => $saving->remaining_money + ($record->price - $data['price'])
+        //             ]);
     
-                    $record->update($data);
-                } else if ($saving->remaining_money >= $data['price']) {
-                    $saving->update([
-                        'remaining_money' => $saving->remaining_money - $data['price']
-                    ]);
+        //             $record->update($data);
+        //         } else if ($saving->remaining_money >= $data['price']) {
+        //             $saving->update([
+        //                 'remaining_money' => $saving->remaining_money - $data['price']
+        //             ]);
     
-                    $record->update($data);
-                } else {
-                    Notification::make()
-                        ->danger()
-                        ->title('The remaining savings selected are insufficient!')
-                        ->body('Choose another savings source that still has a remaining balance.')
-                        ->persistent()
-                        ->send();
+        //             $record->update($data);
+        //         } else {
+        //             Notification::make()
+        //                 ->danger()
+        //                 ->title('The remaining savings selected are insufficient!')
+        //                 ->body('Choose another savings source that still has a remaining balance.')
+        //                 ->persistent()
+        //                 ->send();
     
-                    $this->halt();
-                }
-            }
-        } else {
+        //             $this->halt();
+        //         }
+        //     }
+        // } else {
+
+
             $oldSaving = Savings::find($record->savings_id);
 
-            $oldSaving->update([
-                'remaining_money' => $oldSaving->remaining_money - $data['price']
-            ]);
+            $savingRemainingMoney = $saving->remaining_money - $data['price'];
+            $oldSavingRemainingMoney = $oldSaving->remaining_money + ($data['price'] - $record->price);
+            // dd($savingRemainingMoney, $oldSavingRemainingMoney);
+
+
+            # Jika harga lama tidak sama dengan harga baru
+            if ($record->price != $data['price']) {
+                $hargaLama = $record->price; // harga lama
+                $savingOldSaldo = $oldSaving->remaining_money; // saldo di tabungan lama
+                $hargaBaru = $data['price'];
+    
+                $a = $savingOldSaldo + $hargaLama;
+                $b = $hargaBaru - $hargaLama;
+                $oldSavingRemainingMoney = $a + $b;
+
+            }
+
+            // dd($savingRemainingMoney, $oldSavingRemainingMoney); 
+
+
 
             $saving->update([
-                'remaining_money' => $saving->remaining_money + ($record->price - $data['price'])
+                'remaining_money' => $savingRemainingMoney
+            ]);
+
+            $oldSaving->update([
+                'remaining_money' => $oldSavingRemainingMoney
             ]);
 
             $record->update($data);
-        }
+        // }
 
 
         return $record;
